6ebca2dd1f85b95f9725ffa001df9a8ac5031dbd
Muhammad Huzaifa <mhuzaifasarfraz251@gmail.com>
2026-05-12 16:42:47 +0500

    Update bubble_sort.cpp

--- 12_Bubble_Sort/bubble_sort.cpp
+++ 12_Bubble_Sort/bubble_sort.cpp
@@ -1,33 +1,45 @@
 #include<iostream>
 using namespace std;
+
 void Bubble_Sort(int arr[], int n){
-    for(int i=0; i<n-1; i++){ 
-        for(int j=0; j<n-i-1; j++){ //
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){ 
             if(arr[j]>arr[j+1]){
-                swap(arr[j], arr[j+1]);
+                swap(arr[j], arr[j+1]); 
             }
-        }                         
+        }                        
     }
 }
 
 void Display(int arr[], int n){
-    for(int i=0; i<=n-1; i++){
+    for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
 }
+
 int main(){
     int n;
-    int arr[n];
     cout<<"Enter Array Size: ";
     cin>>n;
-    for(int i=0; i<=n-1; i++){
+    
+    int* arr = new int[n]; 
+    
+    for(int i=0; i<n; i++){
         cout<<"Enter value of Index "<<i<<": ";
         cin>>arr[i];
     }
-    cout<<"Before Sorting"<<endl;
-    Display(arr, n);
-    Bubble_Sort(arr, n);
-    cout<<"\nAfter Sorting"<<endl;
+    
+    cout<<"\nBefore Sorting:"<<endl;
     Display(arr, n);
     
+    Bubble_Sort(arr, n);
+    
+ 
+    cout<<"\n\nAfter Bubble Sorting:"<<endl;
+    Display(arr, n);
+    cout<<endl;
+    
+    delete[] arr; 
+    
+    return 0;
 }
